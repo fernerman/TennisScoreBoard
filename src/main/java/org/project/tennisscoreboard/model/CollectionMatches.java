dd454119fe555d55c6cb06aba27c83d91d4fa1ff
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2025-01-26 17:32:49 +0500

    Добавил коллекцию матчей по ТЗ

---
+++ src/main/java/org/project/tennisscoreboard/model/CollectionMatches.java
@@ -0,0 +1,9 @@
+package org.project.tennisscoreboard.model;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class CollectionMatches {
+
+  public static HashMap<UUID, Score> matches = new HashMap<>();
+}
