811e79256381a8740982ae25b81f3f828587aa2e
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2025-01-26 17:30:20 +0500

    Поправил сервис MatchService

--- src/main/java/org/project/tennisscoreboard/service/MatchService.java
+++ src/main/java/org/project/tennisscoreboard/service/MatchService.java
@@ -1,29 +1,30 @@
 package org.project.tennisscoreboard.service;
 
+import java.sql.SQLException;
+import java.util.Optional;
 import org.project.tennisscoreboard.dao.MatchDao;
 import org.project.tennisscoreboard.dao.PlayerDao;
-import org.project.tennisscoreboard.dto.CreateNewMatchDTO;
+import org.project.tennisscoreboard.exception.PlayerNotFoundException;
 import org.project.tennisscoreboard.model.CollectionMatches;
-
-import java.util.UUID;
+import org.project.tennisscoreboard.model.Match;
+import org.project.tennisscoreboard.model.Player;
 
 public class MatchService {
-    private final PlayerDao playerDao = new PlayerDao();
-    private final MatchDao matchDao = new MatchDao();
 
-    public UUID createMatch(String pitcherName, String hostName) {
-        UUID playerPitcherId = playerDao.create(pitcherName);
-        UUID playerHostId = playerDao.create(hostName);
-        CreateNewMatchDTO createNewMatchDTO = new CreateNewMatchDTO(
-                playerPitcherId,
-                playerHostId,
-                0
-        );
-        UUID uuidMatch = matchDao.create(createNewMatchDTO);
-        CollectionMatches.matches.put(
-                uuidMatch,
-                createNewMatchDTO.getCurrentScore()
-        );
-        return uuidMatch;
+  private final PlayerDao playerDao = new PlayerDao();
+  private final MatchDao matchDao = new MatchDao();
+
+  public Optional<Match> createMatch(String pitcherName, String hostName) throws SQLException {
+    Optional<Player> playerPitcher = playerDao.create(pitcherName);
+    Optional<Player> playerHost = playerDao.create(hostName);
+    if (playerPitcher.isPresent() && playerHost.isPresent()) {
+      Match match = matchDao.create(playerPitcher.get(), playerHost.get());
+      CollectionMatches.matches.put(
+          match.getId(),
+          match.getScore()
+      );
+      return Optional.of(match);
     }
+    throw new PlayerNotFoundException("Player not found");
+  }
 }
