6e7015c81f085a7b5bc2363a870409d4ce3efa81
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2025-02-09 00:31:40 +0500

    MatchService имеет 2 метода Один для текущих матчей, другой для завершённых для DB

--- src/main/java/org/project/tennisscoreboard/service/MatchService.java
+++ src/main/java/org/project/tennisscoreboard/service/MatchService.java
@@ -1,34 +1,63 @@
 package org.project.tennisscoreboard.service;
 
 import java.sql.SQLException;
+import java.util.UUID;
 import org.project.tennisscoreboard.dao.MatchDao;
 import org.project.tennisscoreboard.dao.PlayerDao;
-import org.project.tennisscoreboard.exception.MatchNotCreatedException;
+import org.project.tennisscoreboard.dto.MatchScore;
+import org.project.tennisscoreboard.dto.WinnerLoserDTO;
+import org.project.tennisscoreboard.exception.MatchNotFoundException;
 import org.project.tennisscoreboard.exception.PlayerNotCreatedException;
 import org.project.tennisscoreboard.model.CollectionMatches;
 import org.project.tennisscoreboard.model.Match;
 import org.project.tennisscoreboard.model.Player;
+import org.project.tennisscoreboard.model.Score;
 
 public class MatchService {
 
   private final PlayerDao playerDao = new PlayerDao();
   private final MatchDao matchDao = new MatchDao();
+  private final MatchScoreService matchScoreService = new MatchScoreService();
 
-  public Match createMatch(String pitcherName, String hostName) throws SQLException {
+  public WinnerLoserDTO updateMatchScore(UUID winnerId, MatchScore matchScore)
+      throws SQLException {
+    WinnerLoserDTO winnerLoserDTO = matchScoreService.getWinnerAndLoserScore(
+        winnerId,
+        matchScore).orElseThrow(
+        () -> new MatchNotFoundException("Not found match")
+    );
+    return matchScoreService.getNextScore(winnerLoserDTO);
+  }
+
+  public UUID createCurrentMatch(String pitcherName, String hostName) throws SQLException {
     Player playerPitcher = playerDao.create(pitcherName).orElseThrow(
         () -> new PlayerNotCreatedException("Player not created")
     );
     Player playerHost = playerDao.create(hostName).orElseThrow(
         () -> new PlayerNotCreatedException("Player not created")
     );
+    UUID matchId = UUID.randomUUID();
+    MatchScore matchScore = new MatchScore(playerPitcher.getId(), playerHost.getId(),
+        new Score(), new Score());
+    CollectionMatches.matches.put(matchId, matchScore);
+    return matchId;
+  }
 
-    Match match = matchDao.create(playerPitcher, playerHost).orElseThrow(
-        () -> new MatchNotCreatedException("Match not created")
+  public Match createFinishedMatch(UUID matchId, MatchScore matchScore, UUID winnerId)
+      throws SQLException {
+    CollectionMatches.matches.remove(matchId);
+    Player firstPlayer = playerDao.findById(matchScore.getPlayerPitcherId()).orElseThrow(
+        () -> new PlayerNotCreatedException("Player not found")
     );
-    CollectionMatches.matches.put(
-        match.getId(),
-        match.getScorePitcher()
+    Player secondPlayer = playerDao.findById(matchScore.getPlayerHostId()).orElseThrow(
+        () -> new PlayerNotCreatedException("Player not found")
     );
-    return match;
+    Player winner = playerDao.findById(winnerId).orElseThrow(
+        () -> new PlayerNotCreatedException("Player not found")
+    );
+    return matchDao.create(firstPlayer, secondPlayer, winner)
+        .orElseThrow(
+            () -> new MatchNotFoundException("Match not found")
+        );
   }
 }
