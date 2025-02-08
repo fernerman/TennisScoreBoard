5656acd11aaa49e50aa4477b0f9c49bc5271b856
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2025-02-09 00:24:37 +0500

    Создал коллекцию для текущих матчей

--- src/main/java/org/project/tennisscoreboard/model/CollectionMatches.java
+++ src/main/java/org/project/tennisscoreboard/model/CollectionMatches.java
@@ -2,8 +2,9 @@ package org.project.tennisscoreboard.model;
 
 import java.util.HashMap;
 import java.util.UUID;
+import org.project.tennisscoreboard.dto.MatchScore;
 
 public class CollectionMatches {
 
-  public static HashMap<UUID, Score> matches = new HashMap<>();
+  public static HashMap<UUID, MatchScore> matches = new HashMap<>();
 }
