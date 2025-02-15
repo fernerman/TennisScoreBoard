2bae5ab8cd611cbff44ceea75337f9ce197ac4e0
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2025-02-15 23:02:31 +0500

    feat:added exceptions

---
+++ src/main/java/org/project/tennisscoreboard/exception/MatchNotCreatedException.java
@@ -0,0 +1,8 @@
+package org.project.tennisscoreboard.exception;
+
+public class MatchNotCreatedException extends RuntimeException {
+
+  public MatchNotCreatedException(String message) {
+    super(message);
+  }
+}
