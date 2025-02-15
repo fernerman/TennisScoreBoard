0cee733825fc23c684e9e1a4fb6b11063528491b
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2025-02-15 23:04:12 +0500

    refactor:Use object instead of Optional and get session from service

--- src/main/java/org/project/tennisscoreboard/dao/PlayerDao.java
+++ src/main/java/org/project/tennisscoreboard/dao/PlayerDao.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 import java.util.Optional;
 import java.util.UUID;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.project.tennisscoreboard.model.Player;
 import org.project.tennisscoreboard.util.SessionFactoryUtil;
 
@@ -12,42 +11,33 @@ public class PlayerDao {
 
   private final String GET_NEW_PLAYER_BY_NAME_HQL = "FROM Player e WHERE e.name = :NAME";
 
-  public Optional<Player> create(String username) throws SQLException {
-    Transaction transaction = null;
-    Optional<Player> existPlayer = getPlayerByName(username);
-    if (existPlayer.isPresent()) {
-      return existPlayer;
-    }
+  public Player findOrCreate(Session session, String username) {
 
-    try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
-      transaction = session.beginTransaction();
-      Player player = new Player();
-      player.setName(username);
-      session.persist(player);
-      transaction.commit();
-      return Optional.of(player);
-    } catch (Exception e) {
-      if (transaction != null) {
-        transaction.rollback();
-        throw new SQLException();
-      }
+    Optional<Player> existPlayer = getPlayerByName(session, username);
+    if (existPlayer.isPresent()) {
+      return existPlayer.get();
     }
-    return Optional.empty();
+    Player player = new Player();
+    player.setName(username);
+    session.persist(player);
+    return player;
   }
 
-  public Optional<Player> getPlayerByName(String username) {
-    try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+  public Optional<Player> getPlayerByName(Session session, String username) {
+    try {
       Player player = session.createQuery(GET_NEW_PLAYER_BY_NAME_HQL, Player.class)
           .setParameter("NAME", username)
           .getSingleResult();
       return Optional.ofNullable(player);
+    } catch (Exception e) {
+      return Optional.empty();
     }
   }
 
-  public Optional<Player> findById(UUID id) {
+  public Player findById(UUID id) throws SQLException {
     try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
       Player player = session.find(Player.class, id);
-      return Optional.ofNullable(player);
+      return Optional.ofNullable(player).orElseThrow(() -> new SQLException("Not found player"));
     }
   }
 }
