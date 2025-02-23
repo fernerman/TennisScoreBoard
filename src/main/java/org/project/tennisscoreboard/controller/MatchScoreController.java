1a4f11d4c81e73f023d9c3720362b2d628682731
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2025-02-23 21:06:02 +0500

    refactor:add listener, get services from ServletContext

--- src/main/java/org/project/tennisscoreboard/controller/MatchScoreController.java
+++ src/main/java/org/project/tennisscoreboard/controller/MatchScoreController.java
@@ -1,48 +1,79 @@
 package org.project.tennisscoreboard.controller;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.UUID;
-import org.project.tennisscoreboard.model.Match;
+import org.project.tennisscoreboard.listener.AppContextListener;
 import org.project.tennisscoreboard.model.MatchScore;
 import org.project.tennisscoreboard.service.FinishedMatchesPersistenceService;
 import org.project.tennisscoreboard.service.MatchScoreCalculationService;
 import org.project.tennisscoreboard.service.OngoingMatchesService;
+import org.project.tennisscoreboard.util.JspHelper;
 
 @WebServlet("/match-score/*")
 public class MatchScoreController extends HttpServlet {
 
   public static final String PARAMETER_MATCH_ID = "uuid";
   public static final String PARAMETER_WINNER_ID = "winnerId";
-  private final OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
-  private final MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
-  private final FinishedMatchesPersistenceService finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
+  public static final String ATTRIBUTE_MATCH_ID = "matchUuid";
+  public static final String ATTRIBUTE_MATCH = "match";
+  private OngoingMatchesService ongoingMatchesService;
+  private MatchScoreCalculationService matchScoreCalculationService;
+  private FinishedMatchesPersistenceService finishedMatchesPersistenceService;
 
   @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+  public void init() {
+    ServletContext servletContext = getServletContext();
+    ongoingMatchesService = (OngoingMatchesService) servletContext.getAttribute(
+        AppContextListener.ONGOING_MATCHES_SERVICE);
+    matchScoreCalculationService = (MatchScoreCalculationService) servletContext.getAttribute(
+        AppContextListener.MATCH_SCORE_CALCULATION_SERVICE
+    );
+    finishedMatchesPersistenceService = (FinishedMatchesPersistenceService) servletContext.getAttribute(
+        AppContextListener.FINISHED_MATCHES_PERSISTENCE_SERVICE);
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse resp)
+      throws ServletException, IOException {
+    String matchUuidString = request.getParameter(PARAMETER_MATCH_ID);
+    if (matchUuidString == null) {
+      resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Match ID is required");
+      return;
+    }
+    UUID matchUuid = UUID.fromString(matchUuidString);
+    MatchScore matchScore = ongoingMatchesService.getMatchScore(matchUuid);
+    request.setAttribute(ATTRIBUTE_MATCH, matchScore);
+    request.setAttribute(ATTRIBUTE_MATCH_ID, matchUuid);
+    request.getRequestDispatcher(JspHelper.getPath("match-score")).forward(request, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
     String matchUuidString = request.getParameter(PARAMETER_MATCH_ID);
     String winnerIdByString = request.getParameter(PARAMETER_WINNER_ID);
-    validateUUID(matchUuidString, winnerIdByString);
+    if (matchUuidString == null || winnerIdByString == null) {
+      throw new IllegalArgumentException("Invalid parameter");
+    }
     UUID matchUuid = UUID.fromString(matchUuidString);
     UUID winnerId = UUID.fromString(winnerIdByString);
 
     MatchScore matchScore = ongoingMatchesService.getMatchScore(matchUuid);
-    MatchScore updateMatchScore = matchScoreCalculationService.updateMatchScore(
-        matchScore,
+    MatchScore updateMatchScore = matchScoreCalculationService.updateMatchScore(matchScore,
         winnerId);
+    request.setAttribute(ATTRIBUTE_MATCH, updateMatchScore);
     if (!matchScoreCalculationService.isEndMatchScore(matchScore)) {
-      request.setAttribute("matchResponse", updateMatchScore);
+      response.sendRedirect(request.getContextPath() + "/match-score?uuid=" + matchUuidString);
     } else {
+      finishedMatchesPersistenceService.save(matchScore, winnerId);
       ongoingMatchesService.deleteMatchScore(matchUuid);
-      Match match = finishedMatchesPersistenceService.save(matchScore, matchUuid);
-    }
-  }
-
-  private void validateUUID(String matchUuidString, String winnerIdString) {
-    if (matchUuidString == null || winnerIdString == null) {
-      throw new IllegalArgumentException("Invalid parameter");
+      request.getRequestDispatcher(JspHelper.getPath("final-score")).forward(request, response);
     }
   }
 }
